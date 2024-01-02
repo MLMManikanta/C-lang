1c05fc6a2c08c801a342a064b51ea95660495e39
MLMManikanta <mlmmanikanta@outlook.com>
2024-01-02 19:58:42 +0530

    right triangle

--- Star patterns/right_triangle.c
+++ Star patterns/right_triangle.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 void main(){
     int i,j,n;
-   /* printf("Enter no.of stars in series \n");
-    scanf("%d",&n);*/
-    for(i=1;i<=5;i++)
+   printf("Enter no.of stars in series \n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
     {
-        for(j=1;j<=5;j++){
-            if(i+j<=5){
+        for(j=1;j<=n;j++){
+            if(i+j<=n){
             printf(" ");
             }
             else{
